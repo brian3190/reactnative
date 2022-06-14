deee1d273246dee8b02f71aecdb6a29674258d43
Brian Yap <brian3190@gmail.com>
2022-06-15 00:59:01 +0800

    intial commit

--- App.js
+++ App.js
@@ -61,6 +61,9 @@ export default function App() {
           onPress={handlePressAsync}
         />
       )}
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
     </View>
   );
 }
